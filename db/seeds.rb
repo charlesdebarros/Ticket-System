b4f82f51718d7ba40ddf0b95e8cd7858ed2326ac
Charles De Barros <charlesdebarros@outlook.com>
2021-01-19 21:55:03 +0000

    Added "admin" flag to User model, and improved db/seeds.rb file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,51 +6,72 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(
-  {
-    id: 999,
-    email: 'admin@email.com',
-    password: 'Password123',
-    password_confirmation: 'Password123'
-  }
-)
-
-Project.create(
-  [
+unless User.exists?(email: 'admin@email.com')
+  User.create!(
     {
-      name: 'VSCode',
-      description: 'Code Editing. Redefined!'
-    },
-    {
-      name: 'Atom',
-      description: 'A hackable text editor for the 21st Century'
-    },
-    {
-      name: 'A very, very, very, very, very, very, very, very, very long project name',
-      description: 'A not so long descript though!'
+      id: 998,
+      email: 'admin@email.com',
+      password: 'Password123',
+      password_confirmation: 'Password123',
+      admin: true
     }
-  ]
-)
+  )
+end
+
+unless User.exists?(email: 'viewer@email.com')
+  User.create!(
+    {
+      id: 999,
+      email: 'viewer@email.com',
+      password: 'Password123',
+      password_confirmation: 'Password123'
+    }
+  )
+end
+
+
+%w[VSCode Atom Brackets RubyMine TextMate SublimeText].each do |name|
+  unless Project.exists?(name: name)
+    Project.create!(name: name, description: "A simple sample project about #{name}")
+  end
+end
+
+# Project.create(
+#   [
+#     {
+#       name: 'VSCode',
+#       description: 'Code Editing. Redefined!'
+#     },
+#     {
+#       name: 'Atom',
+#       description: 'A hackable text editor for the 21st Century'
+#     },
+#     {
+#       name: 'A very, very, very, very, very, very, very, very, very long project name',
+#       description: 'A not so long descript though!'
+#     }
+#   ]
+# )
 
 Ticket.create(
   [
     {
       name: 'Issues with auto-formatting code',
       description: 'Auto-formatting not working with PHP files.',
-      project_id: 3,
-      author_id: 999
+      project_id: Project.first.id,
+      author_id: User.first.id
     },
     {
       name: 'Freezing when saving a Javascript file.',
       description: 'The EDI freezes when saving a .js file. Restarted need to open the EDI again.',
-      project_id: 3,
-      author_id: 999
+      project_id: Project.first.id,
+      author_id: User.first.id
     },
     {
       name: 'Not recognising colour theme',
       description: 'Monokai theme not display correctly when using app. Reverts to original theme.',
-      project_id: 3,
-      author_id: 999
+      project_id: Project.first.id,
+      author_id: User.first.id
     }
   ]
 )
