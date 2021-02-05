7d2b4209c3d42c276c4a8cd1977fb2f5b2c2a3c1
Charles De Barros <charlesdebarros@outlook.com>
2021-02-05 19:35:23 +0000

    Only Admin and Managers of a project can edit/update it.

--- spec/policies/project_policy_spec.rb
+++ spec/policies/project_policy_spec.rb
@@ -75,9 +75,40 @@ RSpec.describe ProjectPolicy, type: :policy do
   #   pending "add some examples to (or delete) #{__FILE__}"
   # end
 
-  # permissions :update? do
-  #   pending "add some examples to (or delete) #{__FILE__}"
-  # end
+  permissions :update? do
+    let(:user)    { FactoryBot.create(:user) }
+    let(:project) { FactoryBot.create(:project) }
+
+    it 'blocks anonymous users' do
+      expect(subject).not_to permit(nil, project)
+    end
+
+    it 'does not allow viewers of the project' do
+      assign_role!(user, :viewer, project)
+      expect(subject).not_to permit(user, project)
+    end
+
+    it 'does not allow editors of the project' do
+      assign_role!(user, :editor, project)
+      expect(subject).not_to permit(user, project)
+    end
+
+    it 'allows managers of the project' do
+      assign_role!(user, :manager, project)
+      expect(subject).to permit(user, project)
+    end
+
+    it 'allows admistrators' do
+      admin = FactoryBot.create(:user, :admin)
+      expect(subject).to permit(admin, project)
+    end
+
+    it 'does not allow users assgined to other projects' do
+      other_project = FactoryBot.create(:project)
+      assign_role!(user, :manager, other_project)
+      expect(subject).not_to permit(user, project)
+    end
+  end
 
   # permissions :destroy? do
   #   pending "add some examples to (or delete) #{__FILE__}"
