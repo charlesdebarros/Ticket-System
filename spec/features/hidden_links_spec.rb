848dbc0529da0251aae8d136c21a82738624e9e7
Charles De Barros <charlesdebarros@outlook.com>
2021-02-17 11:12:29 +0000

    Only Managers and Admins can destroy tickets.

--- spec/features/hidden_links_spec.rb
+++ spec/features/hidden_links_spec.rb
@@ -39,14 +39,19 @@ RSpec.feature 'Users can only see the appropriate links:' do
       expect(page).not_to have_link('Delete Project')
     end
 
-    scenario 'cannot see "New Ticket" link' do
+    scenario 'cannot see the "New Ticket" link' do
       visit project_path(project)
       expect(page).not_to have_link('New Ticket')
     end
 
-    scenario 'cannot see "Edit Ticket" link' do
+    scenario 'cannot see the "Edit Ticket" link' do
       visit project_ticket_path(project, ticket)
       expect(page).not_to have_link('Edit Ticket')
+    end
+
+    scenario 'cannot see the "Delete Ticket" link' do
+      visit project_ticket_path(project, ticket)
+      expect(page).not_to have_link('Delete Ticket')
     end
   end
 
@@ -73,9 +78,14 @@ RSpec.feature 'Users can only see the appropriate links:' do
       expect(page).to have_link('New Ticket')
     end
 
-    scenario 'can see "Edit Ticket" link ' do
+    scenario 'can see the "Edit Ticket" link ' do
       visit project_ticket_path(project, ticket)
       expect(page).to have_link('Edit Ticket')
+    end
+
+    scenario 'can see the "Delete Ticket" link' do
+      visit project_ticket_path(project, ticket)
+      expect(page).to have_link('Delete Ticket')
     end
   end
 end
